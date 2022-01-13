182114cf4b8a9809e9fec529edd44d71aac2bad5
булат шакиров <a2009-1@mail.ru>
2022-01-13 11:11:11 +0300

    Добавьте файлы проекта.

---
+++ WebHello/Data/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHello.Data
+{
+    public class Student
+    {
+        public string name;
+        public string surname;
+        public int group;
+        public string faculty;
+
+        public Student(string name, string surname, int group, string faculty)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.group = group;
+            this.faculty = faculty;
+        }
+        //public List<Student> GetStudents()
+        //{
+           // new Student() = { name = "tom", surname="tomov", group=326, faculty="PP" };
+        //}
+    }
+}
